0a9c8ccb3983b95ec3a4c0eee387c68074845d4c
Khaled Reguieg <Khaled.Reguieg@gmail.com>
2021-10-15 15:39:30 +0200

    Inital Commit add cube prefab and corresponding spawner script

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject cubePrefab = null;
+    [Range(1, 200)]
+    [SerializeField] private int cubeSpawnAmount = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            this.SpawnXCubes(this.cubeSpawnAmount);
+        }
+    }
+
+    private void SpawnXCubes(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject.Instantiate(this.cubePrefab);
+        }
+    }
+}
